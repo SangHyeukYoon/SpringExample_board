c0e55f69badf2439f5a73bbe7a6469a0acbc845c
SHYoon <nyoonsh97@gmail.com>
2022-04-18 00:32:19 +0900

    Update modifiedDate.

--- src/main/java/com/yoon/example/springboot/web/dto/BoardsListResponseDto.java
+++ src/main/java/com/yoon/example/springboot/web/dto/BoardsListResponseDto.java
@@ -3,6 +3,7 @@ package com.yoon.example.springboot.web.dto;
 import com.yoon.example.springboot.domain.board.Boards;
 import lombok.Getter;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 
 @Getter
@@ -11,13 +12,13 @@ public class BoardsListResponseDto {
     private Long id;
     private String title;
     private String author;
-    private LocalDateTime modifiedDate;
+    private LocalDate modifiedDate;
 
     public BoardsListResponseDto(Boards entity) {
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
-        this.modifiedDate = entity.getModifiedDate();
+        this.modifiedDate = entity.getModifiedDate().toLocalDate();
     }
 
 }
